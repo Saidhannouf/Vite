c5e2b80a074ad076232db23d922972d4370f5c94
mohammad hijazi <mohammad.hijazi.2000@gmail.com>
2024-04-22 01:24:54 +0300

    fix select

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 // import IndexPage from './IndexPage';
-import IndexPages from './IndexPages';
-import TranslatePage from './TranslatePage';
-
+import IndexPages from "./IndexPages";
+import TranslatePage from "./TranslatePage";
 
 function App() {
   return (
     <BrowserRouter>
-      <>hi
-      <Routes>
-        {/* <Route path="/" exact element={<IndexPage/>}></Route> */}
-       
-        <Route path="/" exact element={<IndexPages/>}></Route>
-        <Route path="/translate" element={<TranslatePage/>}></Route>
-    
-  
-      </Routes></>
+      <>
+        hi
+        <Routes>
+          {/* <Route path="/" exact element={<IndexPage/>}></Route> */}
+
+          <Route path="/" exact element={<IndexPages />}></Route>
+          <Route path="/translate" element={<TranslatePage />}></Route>
+        </Routes>
+      </>
     </BrowserRouter>
   );
 }
 
 export default App;
-
--- src/TranslatePage.js
+++ src/TranslatePage.js
@@ -1,66 +1,82 @@
 // TranslatePage.js
-import React, { useState } from 'react';
-import IndexPages from './IndexPages';
+import React, { useState } from "react";
+import IndexPages, { fetcher } from "./IndexPages";
 
-import axios from 'axios';
+import axios from "axios";
+import useSWR from "swr";
 
-const url = 'https://google-translate1.p.rapidapi.com/language/translate/v2';
+const url = "https://google-translate1.p.rapidapi.com/language/translate/v2";
 
 const headers = {
-  'X-RapidAPI-Key': 'b77cc0403dmshd56eb519180ee3ep135e81jsnd98ccf1c86dc',
-  'X-RapidAPI-Host': 'google-translate1.p.rapidapi.com',
+  "X-RapidAPI-Key": "b77cc0403dmshd56eb519180ee3ep135e81jsnd98ccf1c86dc",
+  "X-RapidAPI-Host": "google-translate1.p.rapidapi.com",
 };
 
-
-
-
-
-
 function TranslatePage() {
-  const [text, setText] = useState('');
-  const [translatedText, setTranslatedText] = useState('');
+  const [text, setText] = useState("");
+  const [translatedText, setTranslatedText] = useState("");
+  const [sourceLang, setSourceLang] = useState('')
+  const [targetLang, setTargetLang] = useState('')
   const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false)
+  const [loading, setLoading] = useState(false);
 
+  const { data, error: errorLanguages} = useSWR(
+    "https://google-translate1.p.rapidapi.com/language/translate/v2/languages",
+    fetcher,
+  );
 
-
-
- 
   const params = new URLSearchParams({
     q: text,
-    source: 'en',
-    target: 'ar'
+    source: sourceLang,
+    target: targetLang,
   });
-  
 
   const handleTranslate = async () => {
+    if(!targetLang){
+      setError('target lang not selected')
+      return
+    }
     try {
-      setLoading(true)
-      setTranslatedText(false)
+      setLoading(true);
+      setTranslatedText(false);
       const response = await axios.post(url, params.toString(), { headers });
       setTranslatedText(response.data.data.translations[0].translatedText);
-      setError("")
+      setError("");
     } catch (error) {
-      setError('Error translating text!');
-    }
-    finally {
-      setLoading(false)
+      setError("Error translating text!");
+    } finally {
+      setLoading(false);
     }
   };
+  if(errorLanguages) return <p>something went wrong!</p>
 
   return (
     <div>
+      <h1>Translation</h1>
+      <div>Select your language</div>
+      <select value={sourceLang} onChange={(e) => setSourceLang(e.target.value)}>
+      <option value={''}>Not selected</option>
+        {data && data.data.languages.map((lang,index) => (
+          <option key={index} value={lang.language}>{lang.language}</option>
+        ))}
+      </select>
 
-
-        <h1>Translation</h1>
-        {<div>Select your language</div>}
-        <IndexPages/>
-         
-     {<div>write your text</div>}
-      <input type="text" value={text} onChange={e => setText(e.target.value)} />
-      {<div>Choose the language you want to translate</div>}
-        <IndexPages/>
-      <button disabled={loading} onClick={handleTranslate}>Translate</button>
+      <div>write your text</div>
+      <input
+        type="text"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+      <div>Choose the language you want to translate</div>
+      <select value={targetLang} onChange={(e) => setTargetLang(e.target.value)}>
+      <option value={''}>Not selected</option>
+        {data && data.data.languages.map((lang,index) => (
+          <option key={index} value={lang.language}>{lang.language}</option>
+        ))}
+      </select>
+      <button disabled={loading} onClick={handleTranslate}>
+        Translate
+      </button>
       {loading && <p>loading ...</p>}
       {translatedText && <div>Translated Text: {translatedText}</div>}
       {error && <div>{error}</div>}
